79a1b12449ac0409d6be68bece4ef30ad52ac40b
sanjaydasss <jebascene1@gmail.com>
2022-11-21 15:15:44 +0530

    commit

---
+++ bin/dart3.dart
@@ -0,0 +1,16 @@
+void main(){
+  String name = "sanjay";
+  int age = 20;
+  int phone = 2255;
+  double mark = 80.5;
+  String email = 'jebascene@gmail.com';
+
+
+  print("my resume");
+  print("my name is $name");
+  print("iam $age yrs old");
+  print("my phone number is $phone");
+  print("my cgpa is $mark");
+  print("my email is $email");
+
+}
--- bin/new_project.dart
+++
@@ -1,3 +0,0 @@
-import 'package:new_project/new_project.dart' as new_project;
-
-void main
